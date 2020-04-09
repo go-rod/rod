acebdab735561f4076a648c722a573cec419ed01
Yad Smood <y.s.inside@gmail.com>
2020-04-09 12:24:23 +0900

    update chromium version

--- lib/launcher/chrome.go
+++ lib/launcher/chrome.go
@@ -18,7 +18,8 @@ import (
 )
 
 // DefaultRevision for chrome
-const DefaultRevision = 748030
+// curl -s -S https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE\?alt\=media
+const DefaultRevision = 757680
 
 // Chrome is a helper to download chrome smartly
 type Chrome struct {
