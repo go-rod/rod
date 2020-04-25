852cc73000a9b7a5e68c18cf99909e7be70dc8c6
Yad Smood <y.s.inside@gmail.com>
2020-04-25 17:49:12 +0900

    doc for contribution

--- browser_test.go
+++ browser_test.go
@@ -13,7 +13,6 @@ func (s *S) TestBrowserPages() {
 	page := s.browser.Page(srcFile("fixtures/click.html"))
 	defer page.Close()
 
-	page.Element("button")
 	pages := s.browser.Pages()
 
 	s.Len(pages, 3)
