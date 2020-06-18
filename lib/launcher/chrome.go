27bc406f7e1366651461e142ff31e5ebc8e52738
Yad Smood <y.s.inside@gmail.com>
2020-06-18 19:01:51 +0900

    update chrome version

--- lib/launcher/chrome.go
+++ lib/launcher/chrome.go
@@ -19,7 +19,7 @@ import (
 
 // DefaultRevision for chrome
 // curl -s -S https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE\?alt\=media
-const DefaultRevision = 737027
+const DefaultRevision = 756035
 
 // HostGoogle to download chrome
 const HostGoogle = "https://storage.googleapis.com"
