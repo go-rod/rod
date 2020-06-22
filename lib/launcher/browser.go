1d4f86d21e21286d64f06472d4b0011fb1912021
NormalPunch <39510490+NormalPunch@users.noreply.github.com>
2020-06-22 13:35:19 +0800

    [add] go generate for fetching the latest chromium version

--- lib/launcher/browser.go
+++ lib/launcher/browser.go
@@ -17,10 +17,6 @@ import (
 	"github.com/ysmood/kit"
 )
 
-// DefaultRevision for browser
-// curl -s -S https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE\?alt\=media
-const DefaultRevision = 756035
-
 // HostGoogle to download browser
 const HostGoogle = "https://storage.googleapis.com"
 
