539ae1df4e922514557dcf86adcf0c1b052ef17d
Yad Smood <y.s.inside@gmail.com>
2020-07-16 01:38:40 +0900

    update chrome's revision

--- lib/launcher/revision.go
+++ lib/launcher/revision.go
@@ -4,4 +4,4 @@ package launcher
 
 // DefaultRevision for chrome
 // curl -s -S https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE\?alt\=media
-const DefaultRevision = 756035
+const DefaultRevision = 768783
