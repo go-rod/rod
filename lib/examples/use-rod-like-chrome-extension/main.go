bba126cec0f4365e457a328132015bf6aabd9c00
Yad Smood <ysmood@users.noreply.github.com>
2020-07-18 19:19:52 +0900

    update doc

--- lib/examples/use-rod-like-chrome-extension/main.go
+++ lib/examples/use-rod-like-chrome-extension/main.go
@@ -8,8 +8,8 @@ import (
 // For example, when you log into your github account, and you want to reuse the login session for automation task.
 // You can use this example to achieve such functionality. Rod will be just like your browser extension.
 func main() {
-	// Launches a new browser with the "new user mode" option, and returns
-	// the URL to control that browser.
+	// Make sure you have closed your browser completely, UserMode can't control a browser that is not launched by it.
+	// Launches a new browser with the "new user mode" option, and returns the URL to control that browser.
 	url := launcher.NewUserMode().Launch()
 
 	browser := rod.New().ControlURL(url).Connect()
