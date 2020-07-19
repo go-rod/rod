d47d75a7e699d80df682f401eee20e9eb948ab15
NormalPunch <39510490+NormalPunch@users.noreply.github.com>
2020-07-19 22:52:14 +0800

    add cookies exporting/importing

--- lib/examples/login-leetcode/main.go
+++ lib/examples/login-leetcode/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/input"
+	"github.com/go-rod/rod/lib/proto"
 )
 
 var username = flag.String("u", "", "user name")
@@ -14,8 +15,19 @@ var password = flag.String("p", "", "password")
 func main() {
 	flag.Parse()
 
+	// This is an example of exporting and importing cookies
+
+	cookies := login()
+
+	reuse(cookies)
+
+}
+
+func login() string {
+
 	// Launch a new browser with default options, and connect to it.
 	browser := rod.New().Connect()
+	defer browser.Close()
 
 	page := browser.Page("https://leetcode.com/accounts/login/")
 
@@ -33,4 +45,33 @@ func main() {
 
 	// print user name
 	fmt.Println(*el.Attribute("title"))
+
+	b, err := json.Marshal(page.Cookies())
+	if err != nil {
+		panic(err)
+	}
+
+	return string(b)
+
+}
+
+func reuse(cookies string) {
+
+	var c []*proto.NetworkCookieParam
+	err := json.Unmarshal([]byte(cookies), &c)
+	if err != nil {
+		panic(err)
+	}
+
+	// Launch a new browser with default options, and connect to it.
+	browser := rod.New().Connect()
+	defer browser.Close()
+
+	page := browser.Page("").SetCookies(c...).Navigate("https://leetcode.com/accounts/login/")
+
+	el := page.Element(".nav-user-icon-base")
+
+	// print user name
+	fmt.Println(*el.Attribute("title"))
+
 }
