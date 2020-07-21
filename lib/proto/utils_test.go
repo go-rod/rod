175e3b737f4a3943be99bd541bf026f8583d01db
Yad Smood <y.s.inside@gmail.com>
2020-07-22 00:50:53 +0900

    add tests for lib/proto

--- lib/proto/utils_test.go
+++ lib/proto/utils_test.go
@@ -39,10 +39,34 @@ func (c *Caller) CallContext() (context.Context, proto.Client, string) {
 	return context.Background(), c.Client, c.Client.sessionID
 }
 
+type normalizeErr struct {
+}
+
+func (n normalizeErr) Normalize() (json.RawMessage, error) {
+	return nil, errors.New("err")
+}
+
+func TestCall(t *testing.T) {
+	err := proto.Call("", normalizeErr{}, "", &Caller{&Client{}})
+	assert.Error(t, err)
+
+	err = proto.Call("", "", "", &Caller{&Client{err: errors.New("err")}})
+	assert.Error(t, err)
+
+	err = proto.Call("", "", func() {}, &Caller{&Client{}})
+	assert.Error(t, err)
+}
+
 func TestE(t *testing.T) {
 	assert.Panics(t, func() {
 		proto.E(errors.New("err"))
 	})
+}
+
+func TestParseMethodName(t *testing.T) {
+	d, n := proto.ParseMethodName("Page.enable")
+	assert.Equal(t, "Page", d)
+	assert.Equal(t, "enable", n)
 }
 
 func TestGetType(t *testing.T) {
@@ -56,6 +80,8 @@ func TestJSON(t *testing.T) {
 	assert.EqualValues(t, 10, j.Int())
 
 	assert.Equal(t, "true", kit.MustToJSON(proto.NewJSON(true)))
+
+	assert.Equal(t, "1 2 3", proto.NewJSON([]int{1, 2, 3}).Join(" "))
 }
 
 func TestTimeCodec(t *testing.T) {
@@ -99,4 +125,12 @@ func TestPatternToReg(t *testing.T) {
 	assert.Equal(t, `\Aa.com/.*/test\z`, proto.PatternToReg("a.com/*/test"))
 	assert.Equal(t, `\A\?\*\z`, proto.PatternToReg(`\?\*`))
 	assert.Equal(t, `\Aa.com\?a=10&b=\*\z`, proto.PatternToReg(`a.com\?a=10&b=\*`))
+}
+
+func TestRect(t *testing.T) {
+	b := &proto.DOMBoxModel{Content: proto.DOMQuad{
+		336, 382, 361, 382, 361, 421, 336, 412,
+	}}
+
+	assert.Equal(t, proto.DOMRect{X: 336, Y: 382, Width: 25, Height: 30}, *b.Rect())
 }
