43915382ee4533f1d520fb8bc65d46159671d798
Yad Smood <ys@ysmood.org>
2020-08-04 11:07:21 +0900

    update browser revision

--- lib/launcher/revision.go
+++ lib/launcher/revision.go
@@ -4,4 +4,4 @@ package launcher
 
 // DefaultRevision for chrome
 // curl -s -S https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE\?alt\=media
-const DefaultRevision = 768783
+const DefaultRevision = 782078
