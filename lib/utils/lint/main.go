329cbcd82c0d29814953564294fb053390175c76
Yad Smood <ys@ysmood.org>
2020-09-20 20:03:56 +0900

    add lint for 'Must' prefixed functions

--- lib/utils/lint/main.go
+++ lib/utils/lint/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"log"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-rod/rod/lib/utils"
 )
@@ -15,4 +20,43 @@ func main() {
 
 	log.Println("godev lint")
 	utils.Exec("godev", "lint")
+
+	lintMustPrefix()
+}
+
+func lintMustPrefix() {
+	log.Println("lint 'Must' prefix")
+
+	paths, err := filepath.Glob("*.go")
+	utils.E(err)
+	lintErr := false
+
+	for _, p := range paths {
+		name := filepath.Base(p)
+		if name == "must.go" || strings.HasSuffix(name, "_test.go") {
+			continue
+		}
+
+		src, err := utils.ReadString(p)
+		utils.E(err)
+
+		list := token.NewFileSet()
+		f, err := parser.ParseFile(list, p, src, 0)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, decl := range f.Decls {
+			fd, ok := decl.(*ast.FuncDecl)
+			if ok && strings.HasPrefix(fd.Name.Name, "Must") {
+				log.Printf("%s %s\n", list.Position(fd.Name.Pos()), fd.Name.Name)
+				lintErr = true
+			}
+		}
+		break
+	}
+
+	if lintErr {
+		log.Fatalln("'Must' prefixed function should be declared in file 'must.go'")
+	}
 }
