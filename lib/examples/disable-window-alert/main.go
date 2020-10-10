f63387d5c39702656947c2947117396d5dca60cb
xuews <xuews@hsmap.com>
2020-10-10 17:58:21 +0800

    Disable window alerts when the page loads before

--- lib/examples/disable-window-alert/main.go
+++ lib/examples/disable-window-alert/main.go
@@ -2,27 +2,54 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
 )
 
+const (
+	indexHTML = `<!doctype html>
+<html>
+<body>
+  
+<script language="javascript" type="text/javascript">
+alert("信息不存在");
+</script>
+
+</body>
+</html>`
+)
+
+// Server creates a simple HTTP server that pop-up alert .
+func Server(addr string) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		_, _ = fmt.Fprintf(res, indexHTML)
+	})
+	_ = http.ListenAndServe(addr, mux)
+}
+
+var port = 8544
+var host = "http://localhost:8544"
+
 func main() {
-	page := rod.New().MustConnect().MustPage("https://teslainventory.teslastats.no/")
+	// start cookie server
+	go Server(fmt.Sprintf(":%d", port))
 
+	url, _ := launcher.New().Headless(false).Launch()
+	browser := rod.New().ControlURL(url).MustConnect()
+
+	// Creating a Page Object
+	page, _ := browser.Page("")
+
+	// Evaluates given script in every frame upon creation
 	// Disable all alerts by making window.alert no-op.
-	page.MustEval(`window.alert = () => {}`)
+	page.MustEvalOnNewDocument(`window.alert = () => {}`)
 
-	// Navigate through country, model options whitout worrying about alert messages.
-	country := page.MustElement("#car_list_country")
-	model := page.MustElement("#car_list_model")
+	// Navigate to the website you want to visit
+	page.Navigate(host)
 
-	for _, c := range page.MustElements("#car_list_country option") {
-		country.MustSelect(c.MustText())
+	fmt.Println(page.MustElement("script").Text())
 
-		for _, m := range page.MustElements("#car_list_model option") {
-			model.MustSelect(m.MustText())
-
-			fmt.Println(c.MustText(), m.MustText(), "selected.")
-		}
-	}
 }
