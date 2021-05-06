31e67c25eea26d7823a15d37511d40ed968b338c
Benji (bbordel) <bbordel@sfbx.io>
2021-05-06 23:11:15 +0200

    Fix CI (replace loop with append destructure)

--- lib/launcher/os_unix.go
+++ lib/launcher/os_unix.go
@@ -15,12 +15,9 @@ func (l *Launcher) osSetupCmd(cmd *exec.Cmd) {
 	if flags, has := l.GetFlags(flagXVFB); has {
 		var command []string
 		// flags must append before cmd.Args
-		for _, flag := range flags {
-			command=append(command,flag)
-		}
-		for _, arg := range cmd.Args {
-			command=append(command,arg)
-		}
+		command = append(command, flags...)
+		command = append(command, cmd.Args...)
+
 		*cmd = *exec.Command("xvfb-run", command...)
 	}
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
