179fc8f7eaf63c9974456db38fdea704f1bd186b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-06-16 15:45:31 -0400

    Fix translator example
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- lib/examples/translator/main.go
+++ lib/examples/translator/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"log"
+	"strings"
 
 	"github.com/go-rod/rod"
 )
 
 func main() {
-	// get the first commandline argument
-	source := os.Args[1]
+	flag.Parse()
+
+	// get the commandline arguments
+	source := strings.TrimSpace(strings.Join(flag.Args(), " "))
+	if source == "" {
+		log.Fatal("usage: go run main.go -- 'This is the phrase to translate to Spanish.'")
+	}
 
 	browser := rod.New().MustConnect()
 
-	page := browser.MustPage("https://translate.google.com")
+	page := browser.MustPage("https://translate.google.com/?sl=auto&tl=es&op=translate")
 
 	el := page.MustElement(`textarea[aria-label="Source text"]`)
 
