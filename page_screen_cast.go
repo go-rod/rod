1850fc284a793a11c697b719a9628a6187ed9042
L <L@abc.com>
2022-07-05 19:58:52 +0800

    add screen cast mp4

--- page_screen_cast.go
+++ page_screen_cast.go
@@ -2,7 +2,11 @@ package rod
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
 	"sort"
+	"strconv"
 	"time"
 
 	"github.com/TommyLeng/go-rod/lib/proto"
@@ -20,10 +24,9 @@ type VideoFrame struct {
 
 // ScreenCastRecord listen PageScreenCastFrame and convert it directly into AVI Movie
 func (p *Page) ScreenCastRecordAvi(videoAVIPath string, videoFrames *[]VideoFrame, fps int) (*mjpeg.AviWriter, error) {
-	browserBound, errorBrowserBound := p.GetWindow()
-
-	if errorBrowserBound != nil {
-		return nil, errorBrowserBound
+	browserBound, err := p.GetWindow()
+	if err != nil {
+		return nil, err
 	}
 
 	aviWriter, err := mjpeg.New(videoAVIPath, int32(*browserBound.Width), int32(*browserBound.Height), int32(fps))
@@ -67,7 +70,7 @@ func (p *Page) ScreenCastRecordAvi(videoAVIPath string, videoFrames *[]VideoFram
 	return &aviWriter, nil
 }
 
-func (p *Page) ScreenCastStartAvi(JPEGQuality int) error {
+func (p *Page) ScreenCastStart(JPEGQuality int) error {
 	everyNthFrame := 1
 	return proto.PageStartScreencast{
 		Format:        proto.PageStartScreencastFormatJpeg,
@@ -88,6 +91,7 @@ func (p *Page) ScreenCastStopAvi(aviWriter *mjpeg.AviWriter, videoFrames *[]Vide
 		return vfs[i].Timestamp.Before(vfs[y].Timestamp)
 	})
 
+	//screen cast frames may not has the same fps, so convert to our fps
 	for i, vf := range vfs {
 		if i > 0 {
 			dur := float64(vf.Timestamp.Sub(vfs[i-1].Timestamp).Nanoseconds())/float64(time.Second) + vfs[i-1].AccumDurationInSecond
@@ -124,6 +128,172 @@ func (p *Page) ScreenCastStopAvi(aviWriter *mjpeg.AviWriter, videoFrames *[]Vide
 	aw.Close()
 
 	fmt.Println("totalFrameCnt: ", total)
+
+	return nil
+}
+
+// ScreenCastRecord listen PageScreenCastFrame and convert it directly into MP4 using ffmpeg
+func (p *Page) ScreenCastRecordMp4(videoFrames *[]VideoFrame) error {
+	go p.EachEvent(func(e *proto.PageScreencastFrame) {
+		err := proto.PageScreencastFrameAck{
+			SessionID: e.SessionID,
+		}.Call(p)
+		if err != nil {
+			fmt.Println("ScreencastFrameAck err:", err)
+		}
+
+		*videoFrames = append(*videoFrames, VideoFrame{
+			Data:      e.Data,
+			Timestamp: e.Metadata.Timestamp.Time(),
+		})
+	})()
+
+	return nil
+}
+
+// use ffmpeg to create mp4 directly using pipe
+func (p *Page) ScreenCastStopMp4UsingPipe(videoFrames *[]VideoFrame, outputFile string, fps int) error {
+	err := proto.PageStopScreencast{}.Call(p)
+	if err != nil {
+		return err
+	}
+
+	vfs := *videoFrames
+
+	sort.Slice(vfs, func(i int, y int) bool {
+		return vfs[i].Timestamp.Before(vfs[y].Timestamp)
+	})
+
+	pr, pw := io.Pipe()
+
+	//cat $(find . -maxdepth 1 -name '*.png' -print | sort | tail -10) | ffmpeg -framerate 25 -i - -vf format=yuv420p -movflags +faststart output.mp4
+
+	cmd := exec.Command("ffmpeg", "-y", // Yes to all
+		"-i", "pipe:0", // take stdin as input
+		//"-filter:v", "fps=25",
+		"-vf", "format=yuv420p",
+		//"-movflags", "+faststart",
+		outputFile, // output
+	)
+	cmd.Stderr = os.Stderr // bind log stream to stderr
+	cmd.Stdin = pr
+
+	err = cmd.Start() // Start a process on another goroutine
+	if err != nil {
+		return err
+	}
+
+	for _, v := range vfs {
+		pw.Write(v.Data)
+	}
+
+	err = pw.Close()
+	if err != nil {
+		return err
+	}
+	err = pr.Close() // close the stdin, or ffmpeg will wait forever
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Wait() // wait until ffmpeg finish
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// use ffmpeg to create mp4
+func (p *Page) ScreenCastStopMp4(videoFrames *[]VideoFrame, outputFile string) error {
+	err := proto.PageStopScreencast{}.Call(p)
+	if err != nil {
+		return err
+	}
+
+	vfs := *videoFrames
+	fps := 50
+	fpsStr := strconv.FormatInt(int64(fps), 10)
+
+	sort.Slice(vfs, func(i int, y int) bool {
+		return vfs[i].Timestamp.Before(vfs[y].Timestamp)
+	})
+
+	//screen cast frames may not has the same fps, so convert to 50 fps, we feel better when make use 50 fps
+	for i, vf := range vfs {
+		if i > 0 {
+			dur := float64(vf.Timestamp.Sub(vfs[i-1].Timestamp).Nanoseconds())/float64(time.Second) + vfs[i-1].AccumDurationInSecond
+			fc := (dur * float64(fps)) + vfs[i-1].FrameCntRemaining
+			fci := float64(int64(fc))
+			vfs[i-1].DurationInSecond = dur
+			vfs[i-1].FrameCnt = fci
+
+			// if frame count = 0, save the duration to current frame's AccumDurationInSecond
+			if fci == float64(0) {
+				vfs[i].AccumDurationInSecond += dur
+				continue
+			}
+
+			fcr := fc - fci
+			// save the remaining frame count portion to current frame
+			if fcr > 0 {
+				vfs[i].FrameCntRemaining += fcr
+			}
+		}
+	}
+
+	//cat $(find . -maxdepth 1 -name '*.png' -print | sort | tail -10) | ffmpeg -framerate 25 -i - -vf format=yuv420p -movflags +faststart output.mp4
+
+	// I dunno why I cannot change frame rate to 50 fps, it still make use 25 fps, so I speed the video up using setpts=0.5*PTS in order to achieve 50 fps
+	cmd := exec.Command("ffmpeg", "-y", // Yes to all
+		"-i", "pipe:0", // take stdin as input
+		//"-framerate", fpsStr,
+		"-r", fpsStr,
+		"-filter:v", "setpts=0.5*PTS",
+		//"-f", "image2pipe", //not working
+		//"-f", "rawvideo",
+		//"-filter:v", "fps=" + fpsStr,
+		//"-c:v", "libx264",
+		//"-vf", "format=yuv420p",
+		//"-movflags", "+faststart",
+		outputFile, // output
+	)
+	cmd.Stderr = os.Stderr // bind log stream to stderr
+
+	stdin, err := cmd.StdinPipe() // Open stdin pip
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Start() // Start a process on another goroutine
+	if err != nil {
+		return err
+	}
+
+	total := 0
+	for j, vf := range vfs {
+		fmt.Printf("frame %d, data %d, duration %v,\t\tdurationAcc %v,\t\tframeCnt %v\t\tframeCntR %v\n", j, len(vf.Data), vf.DurationInSecond, vf.AccumDurationInSecond, vf.FrameCnt, vf.FrameCntRemaining)
+		if vf.FrameCnt > 0 {
+			for i := int64(0); i < int64(vf.FrameCnt); i++ {
+				_, err = stdin.Write(vf.Data)
+				if err != nil {
+					return err
+				}
+				total += 1
+			}
+		}
+	}
+	fmt.Printf("totalFrameCnt: %v\n\n", total)
+
+	err = stdin.Close()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Wait() // wait until ffmpeg finish
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
--- page_screen_cast_test.go
+++ page_screen_cast_test.go
@@ -2,20 +2,22 @@ package rod_test
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
 	"testing"
 	"time"
 
 	"github.com/TommyLeng/go-rod"
+	"github.com/TommyLeng/go-rod/lib/proto"
 )
 
 //GODEBUG="tracebackancestors=1000" go test -timeout 30s -run ^TestPageScreenCastAvi$ github.com/TommyLeng/go-rod -v -count=1 -parallel=1
 func TestPageScreenCastAvi(t *testing.T) {
-	g := setup(t)
+	//g := setup(t)
 
 	{
-		b := g.browser.MustConnect()
-		defer b.MustClose()
-
+		b := rod.New().MustConnect()
 		p := b.MustPage("https://www.google.com").MustWaitLoad()
 
 		time.Sleep(10 * time.Second)
@@ -24,102 +26,247 @@ func TestPageScreenCastAvi(t *testing.T) {
 		videoFrames := []rod.VideoFrame{}
 		fps := 25
 
-		// ScreenCastRecord listen PageScreenCastFrame and convert it directly into AVI Movie
-		aviWriter, errorRecord := p.ScreenCastRecordAvi("sample.avi", &videoFrames, fps) // Only support .avi video file & frame per second
-		if errorRecord != nil {
-			t.Fatal(errorRecord)
+		// ScreenCastRecord listen PageScreenCastFrame and save data into videoFrames
+		aviWriter, err := p.ScreenCastRecordAvi("sample.avi", &videoFrames, fps) // Only support .avi video file & frame per second
+		if err != nil {
+			t.Fatal(err)
 		}
 
 		// ScreenCastStart start listening ScreenCastRecord
-		errorStart := p.ScreenCastStartAvi(100) // Image quality & frame per second
-		if errorStart != nil {
-			t.Fatal(errorStart)
+		err = p.ScreenCastStart(100) // Image quality
+		if err != nil {
+			t.Fatal(err)
 		}
 
 		fmt.Println("sleep 10 seconds")
-		time.Sleep(10 * time.Second)
+		time.Sleep(3 * time.Second)
 
-		p.Navigate("https://member.bbtb.dev")
+		p.Navigate("https://www.youtube.com")
 
-		// ScreenCastStop stop listening ScreenCastRecord
-		errorStop := p.ScreenCastStopAvi(aviWriter, &videoFrames, fps)
-		if errorStop != nil {
-			t.Fatal(errorStop)
+		time.Sleep(7 * time.Second)
+
+		// ScreenCastStop stop listening ScreenCastRecord and convert the videoFrames data into avi file
+		err = p.ScreenCastStopAvi(aviWriter, &videoFrames, fps)
+		if err != nil {
+			t.Fatal(err)
 		}
 
 		p.MustClose()
+		b.MustClose()
 	}
 }
 
 //GODEBUG="tracebackancestors=1000" go test -timeout 60s -run ^TestPageScreenCastAvi2$ github.com/TommyLeng/go-rod -v -count=1
 func TestPageScreenCastAvi2(t *testing.T) {
-	g := setup(t)
+	//g := setup(t)
 
-	{
-		browser := g.browser.MustConnect()
+	browser := rod.New().MustConnect()
 
-		page := browser.MustPage("https://member.bbtb.dev")
+	page := browser.MustPage("https://member.bbtb.dev")
 
-		page.MustEval(`k => localStorage[k] = true`, "hasEnterBefore")
+	GoToTestPage(browser, page)
 
-		//page.WaitElementsMoreThan(".TermAndConditionCheckbox-agree-tnc-checkbox-bhCdR", 0)
-		l := page.MustElement(`.TermAndConditionCheckbox-agree-tnc-checkbox-bhCdR`)
+	//page.MustScreenshot("a.png")
 
-		time.Sleep(2 * time.Second)
-		fmt.Println("1")
+	videoFrames := []rod.VideoFrame{}
+	fps := 50
 
-		l.MustElement("label").MustWaitStable().MustClick()
-		fmt.Println("2")
-
-		page.MustElement(".LoginForm-visitor-button-uLUAT").MustClick()
-		time.Sleep(2 * time.Second)
-		fmt.Println("3")
-
-		page.MustElement("#captcha").MustInput("11111")
-		fmt.Println("4")
-
-		time.Sleep(2 * time.Second)
-		page.MustElement(".CaptchaModal-button-group-YP5iw button").MustClick()
-		fmt.Println("5")
-
-		time.Sleep(3 * time.Second)
-		page.MustElement(".BetTable-game-instance-OBAbO").MustClick()
-		time.Sleep(3 * time.Second)
-		fmt.Println("6")
-
-		//page.MustScreenshot("a.png")
-
-		videoFrames := []rod.VideoFrame{}
-		fps := 50
-
-		// ScreenCastRecord listen PageScreenCastFrame and convert it directly into AVI Movie
-		aviWriter, errorRecord := page.ScreenCastRecordAvi("sample.avi", &videoFrames, fps) // Only support .avi video file & frame per second
-		if errorRecord != nil {
-			g.Fatal(errorRecord)
-		}
-		/*
-			errorRecord := page.ScreenCastJPGRecord()
-			if errorRecord != nil {
-				t.Fatal(errorRecord)
-			}
-		*/
-
-		// ScreenCastStart start listening ScreenCastRecord
-		errorStart := page.ScreenCastStartAvi(100) // Image quality & frame per second
-		if errorStart != nil {
-			g.Fatal(errorStart)
-		}
-
-		fmt.Println("sleep 10 seconds")
-		time.Sleep(10 * time.Second)
-
-		// ScreenCastStop stop listening ScreenCastRecord
-		errorStop := page.ScreenCastStopAvi(aviWriter, &videoFrames, fps)
-		if errorStop != nil {
-			g.Fatal(errorStop)
-		}
-
-		page.MustClose()
-		browser.MustClose()
+	// ScreenCastRecord listen PageScreenCastFrame and save data into videoFrames
+	aviWriter, err := page.ScreenCastRecordAvi("sample.avi", &videoFrames, fps) // Only support .avi video file & frame per second
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	// ScreenCastStart start listening ScreenCastRecord
+	err = page.ScreenCastStart(100) // Image quality
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("sleep 10 seconds")
+	time.Sleep(10 * time.Second)
+
+	// ScreenCastStop stop listening ScreenCastRecord and convert the videoFrames data into avi file
+	err = page.ScreenCastStopAvi(aviWriter, &videoFrames, fps)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	page.MustClose()
+	browser.MustClose()
+}
+
+//Direct put data from screen cast event to ffmpeg stdin, but the result is not good, the video is not smooth and it is more than 10 seconds
+//GODEBUG="tracebackancestors=1000" go test -timeout 60s -run ^TestPageScreenCastDirectMp4$ github.com/TommyLeng/go-rod -v -count=1
+func TestPageScreenCastDirectMp4(t *testing.T) {
+	//g := setup(t)
+
+	browser := rod.New().MustConnect()
+
+	page := browser.MustPage("https://member.bbtb.dev")
+
+	GoToTestPage(browser, page)
+
+	pr, pw := io.Pipe()
+
+	go page.EachEvent(func(e *proto.PageScreencastFrame) {
+		err := proto.PageScreencastFrameAck{
+			SessionID: e.SessionID,
+		}.Call(page)
+		if err != nil {
+			fmt.Println("ScreencastFrameAck err:", err)
+		}
+		pw.Write(e.Data)
+	})()
+
+	//cat $(find . -maxdepth 1 -name '*.png' -print | sort | tail -10) | ffmpeg -framerate 25 -i - -vf format=yuv420p -movflags +faststart output.mp4
+
+	cmd := exec.Command("ffmpeg", "-y", // Yes to all
+		"-i", "pipe:0", // take stdin as input
+		"-vf", "format=yuv420p",
+		"-movflags", "+faststart",
+		"output.mp4", // output
+	)
+	cmd.Stderr = os.Stderr // bind log stream to stderr
+	cmd.Stdin = pr
+
+	err := cmd.Start() // Start a process on another goroutine
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	everyNthFrame := 1
+	qty := 100
+	proto.PageStartScreencast{
+		Format:        proto.PageStartScreencastFormatJpeg,
+		Quality:       &qty,
+		EveryNthFrame: &everyNthFrame,
+	}.Call(page)
+
+	time.Sleep(10 * time.Second)
+
+	err = proto.PageStopScreencast{}.Call(page)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = pw.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = pr.Close() // close the stdin, or ffmpeg will wait forever
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = cmd.Wait() // wait until ffmpeg finish
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	page.MustClose()
+	browser.MustClose()
+}
+
+//GODEBUG="tracebackancestors=1000" go test -timeout 60s -run ^TestPageScreenCastMp4UsingPipe$ github.com/TommyLeng/go-rod -v -count=1
+func TestPageScreenCastMp4UsingPipe(t *testing.T) {
+	//g := setup(t)
+
+	browser := rod.New().MustConnect()
+
+	page := browser.MustPage("https://member.bbtb.dev")
+
+	GoToTestPage(browser, page)
+
+	videoFrames := []rod.VideoFrame{}
+
+	// ScreenCastRecord listen PageScreenCastFrame and save data into videoFrames
+	err := page.ScreenCastRecordMp4(&videoFrames)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ScreenCastStart start listening ScreenCastRecord
+	err = page.ScreenCastStart(100) // Image quality & frame per second
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("sleep 10 seconds")
+	time.Sleep(10 * time.Second)
+
+	// ScreenCastStop stop listening ScreenCastRecord and convert the videoFrames data into mp4 file
+	err = page.ScreenCastStopMp4UsingPipe(&videoFrames, "output.mp4", 25)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	page.MustClose()
+	browser.MustClose()
+}
+
+//GODEBUG="tracebackancestors=1000" go test -timeout 60s -run ^TestPageScreenCastMp4$ github.com/TommyLeng/go-rod -v -count=1
+func TestPageScreenCastMp4(t *testing.T) {
+	//g := setup(t)
+
+	browser := rod.New().MustConnect()
+
+	page := browser.MustPage("https://member.bbtb.dev")
+
+	GoToTestPage(browser, page)
+
+	videoFrames := []rod.VideoFrame{}
+
+	// ScreenCastRecord listen PageScreenCastFrame and save data into videoFrames
+	err := page.ScreenCastRecordMp4(&videoFrames)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ScreenCastStart start listening ScreenCastRecord
+	err = page.ScreenCastStart(100) // Image quality & frame per second
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("sleep 10 seconds")
+	time.Sleep(10 * time.Second)
+
+	// ScreenCastStop stop listening ScreenCastRecord and convert the videoFrames data into mp4 file
+	err = page.ScreenCastStopMp4(&videoFrames, "output.mp4")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	page.MustClose()
+	browser.MustClose()
+}
+
+func GoToTestPage(browser *rod.Browser, page *rod.Page) {
+	page.MustEval(`k => localStorage[k] = true`, "hasEnterBefore")
+
+	//page.WaitElementsMoreThan(".TermAndConditionCheckbox-agree-tnc-checkbox-bhCdR", 0)
+	l := page.MustElement(`.TermAndConditionCheckbox-agree-tnc-checkbox-bhCdR`)
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("1")
+
+	l.MustElement("label").MustWaitStable().MustClick()
+	fmt.Println("2")
+
+	page.MustElement(".LoginForm-visitor-button-uLUAT").MustClick()
+	time.Sleep(2 * time.Second)
+	fmt.Println("3")
+
+	page.MustElement("#captcha").MustInput("11111")
+	fmt.Println("4")
+
+	time.Sleep(2 * time.Second)
+	page.MustElement(".CaptchaModal-button-group-YP5iw button").MustClick()
+	fmt.Println("5")
+
+	time.Sleep(3 * time.Second)
+	page.MustElement(".BetTable-game-instance-OBAbO").MustClick()
+	time.Sleep(3 * time.Second)
+	fmt.Println("6")
 }
