a2b23b067f00e7280598c627b27e26ab822e2bb6
Yad Smood <y.s.inside@gmail.com>
2022-08-27 11:38:37 +0800

    update golangci-lint version

--- lib/utils/lint/main.go
+++ lib/utils/lint/main.go
@@ -19,7 +19,7 @@ func main() {
 
 	utils.Exec("npx -ys -- prettier@2.5.1 --loglevel=error --config=lib/utils/lint/prettier.yml --write --ignore-path=.gitignore .")
 
-	utils.Exec("go run github.com/ysmood/golangci-lint@latest")
+	utils.Exec("go run github.com/ysmood/golangci-lint@v0.6.0")
 
 	lintMustPrefix()
 
