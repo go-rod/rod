bac8ee99a3cb30143a93305c37de315541b29bfa
Mzack9999 <mzack9999@protonmail.com>
2022-11-23 22:39:23 +0100

    use proxy to download chrome binary

--- lib/launcher/launcher.go
+++ lib/launcher/launcher.go
@@ -304,6 +304,7 @@ func (l *Launcher) RemoteDebuggingPort(port int) *Launcher {
 
 // Proxy for the browser
 func (l *Launcher) Proxy(host string) *Launcher {
+	l.browser.Proxy(host)
 	return l.Set(flags.ProxyServer, host)
 }
 
